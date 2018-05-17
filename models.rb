b976780762743b030b0acfb5b4c7497395c8fc04
Sam. O <samrey2018@gmail.com>
2018-05-17 14:26:02 +0000

    automatically backed up by learn

--- models.rb
+++ models.rb
@@ -18,15 +18,15 @@ end
 
 class Course
   attr_reader :name, :topic
- 
+
   COURSES = []
- 
+
   def initialize(args)
     @name = args[:name]
     @topic = args[:topic]
     COURSES << self
   end
- 
+
   def self.all
     COURSES
   end
